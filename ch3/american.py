19164f4ad9d979e6c280804c05240bb536cf5970
Dima <dima.tumanenko@gmail.com>
2021-10-11 21:54:16 +0300

    First commit after test

--- ch3/american.py
+++ ch3/american.py
@@ -8,3 +8,4 @@ wm.add('Central America', ['bz', 'cr', 'gt', 'hn', 'ni', 'pa', 'sv'])
 wm.add('South America', ['ar', 'bo', 'br', 'cl', 'co', 'ec', 'gf', 'gy', 'pe', 'py', 'sr', 'uy', 've'])
 
 wm.render_to_file('american.svg')
+"""Тест америки"""
